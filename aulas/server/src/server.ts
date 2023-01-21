d7d67b564097c00e3dba4b5c0bbcbcb6b54861c3
Ramon Barreto Medrado <105830248+RamonBarret@users.noreply.github.com>
2023-01-21 18:18:18 -0300

    aula02-commits

--- aulas/server/src/server.ts
+++ aulas/server/src/server.ts
@@ -1,23 +1,14 @@
 import Fastify from "fastify";
 import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from "./routes";
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 /*access permitions - app.register(cors, {
     origin: ['http://localhost:3000']
 }) */
 app.register(cors) 
-
-/* 
-   * Método HTTP: Get, Post, Put, Patch, Delete
-*/
-
-app.get('/', async () => {
-    const habits = await prisma.habit.findMany()
-    return habits
-})
+app.register(appRoutes)
 
 app.listen({
     port: 3333,
